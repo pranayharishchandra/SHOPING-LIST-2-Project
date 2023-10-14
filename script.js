a575cf82a779da7359bbdd01a74974ca64e2d61d
pranay <pranayharishchandra@gmail.com>
2023-10-14 13:29:12 +0530

    starting js

---
+++ script.js
@@ -0,0 +1,11 @@
+ const itemForm  =  document.getElementById('item-form')
+ const itemInput =  document.getElementById('item-input')
+ const itemList  = document.getElementById('item-list')
+
+function addItem() {
+    
+}
+
+itemForm.addEventListener('submit',  addItem)
+
+ 
